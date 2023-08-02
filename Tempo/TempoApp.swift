4786b767d96c24277b465ad439cdcab6a663c6a6
Duff Neubauer <d.neubauer@me.com>
2023-08-02 12:45:38 -0700

    Initial Commit

---
+++ Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Duff Neubauer on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
